97b5cd72f3999df3d53097d0c2702ff683a5aa79
CorporateGuuu <FitzAmani@outlook.com>
2025-09-12 15:05:50 -0400

    Fix import path for purchaseOrderUtils.js in purchaseorders API to resolve Netlify build error

--- pages/api/purchaseorders.js
+++ pages/api/purchaseorders.js
@@ -1,4 +1,4 @@
-const { getPurchaseOrders, validateApiKey, getMockPurchaseOrders, validateParams } = require('../../../utils/purchaseOrderUtils.js');
+const { getPurchaseOrders, validateApiKey, getMockPurchaseOrders, validateParams } = require('../../utils/purchaseOrderUtils.js');
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
